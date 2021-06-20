c2bfe5aecb58b448d8b20a5f2c0d244bfcf3813d
keilacardodo <keila_dcardoso@hotmail.com>
2021-06-20 11:12:37 -0300

    Tabuada09

---
+++ Tabuada/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, numero = 1;
+
+            while (i <= 10)
+            {
+                int tabuada = 9 * numero;
+                Console.WriteLine(tabuada); //tabuada do 9
+                numero = numero + 1;
+                i++;
+            }
+        }
+    }
+}
